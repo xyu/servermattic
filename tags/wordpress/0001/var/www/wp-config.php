3bcdb7830c559917ff435871109bed99264c60ec
Xiao Yu <xyu@a8c.com>
2016-09-17 20:43:10 +0000

    Update db password

--- tags/wordpress/0001/var/www/wp-config.php
+++ tags/wordpress/0001/var/www/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wpdb');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'god');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
